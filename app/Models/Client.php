d0c1c95b56ff8472718207ecee4b479611e867c1
rgrttg <ottiger@outlook.de>
2024-05-14 16:52:31 +0200

    php make model Client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    // Kolonnen beschreibbar machen
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'email',
+        'phone',
+        'subject',
+        'street',
+        'city',
+        'birthdate'
+    ];    
 }
