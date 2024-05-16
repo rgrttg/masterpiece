9bf2ab305fcd9d40a0d31b3f3c53b94b0513dce5
rgrttg <ottiger@outlook.de>
2024-05-16 12:11:24 +0200

    make model Appointment

---
+++ app/Models/Appointment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    // Version 1 aus:
+    // https://laraveldaily.com/post/laravel-appointment-calendar-fullcalendar-demo
+    protected $fillable = [
+        'start_time',
+        'finish_time',
+        'comments',
+        'client_id',
+        'employee_id',
+    ];
+ 
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+ 
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
---
+++ database/migrations/2024_05_16_094753_create_appointments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            // Version 1 aus:
+            // https://laraveldaily.com/post/laravel-appointment-calendar-fullcalendar-demo
+            $table->datetime('start_time');
+            $table->datetime('finish_time');
+            $table->longText('comments')->nullable();
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('employee_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
