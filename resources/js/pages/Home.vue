6507a51ec2e48ac291cd8107e9d3ab170688986e
rgrttg <ottiger@outlook.de>
2024-05-22 11:30:21 +0200

    resources hinzugefügt

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+    <!-- Header -->
+
+    <!-- Main -->
+    <div>
+        <h1>Startseite</h1>
+        <router-link to="/login">Link zur Login Seite</router-link>
+    </div>
+
+    <!-- Footer -->
+</template>
+
+<style lang="scss">
+h1 {
+    color: red;
+}</style>
---
+++ resources/js/router.js
@@ -0,0 +1,30 @@
+
+
+
+export const routes = [
+    {
+        path: "/",
+        component: () => import("./pages/Home.vue"),
+    },
+    
+    {
+        path: "/login",
+        component: () => import("./pages/Auth/Login.vue"),
+    },
+
+    {
+        path: "/dashboard",
+        component: () => import("./pages/Dashboard.vue"),
+        meta: { requiresAuth: true },
+    },
+
+    {
+        path: "/home",
+        component: () => import("./pages/Home.vue"),
+    },
+];
+
+
+
+
+
