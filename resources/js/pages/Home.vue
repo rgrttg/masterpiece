177db644e03ed2f42741539b5b894a6b2f84bde4
rgrttg <ottiger@outlook.de>
2024-05-22 16:34:51 +0200

    class haupt

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -6,28 +6,32 @@ import AppFooter from '../components/AppFooter.vue';
 </script>
 
 <template>
-<!-- Header -->
-<header>
-  <AppHeader />
-</header>
-
-<!-- Main -->
-<main>
-  <div>
-    <h1>Deine Kinesiologie-Praxis in Hedingen und Urdorf</h1>
-    <router-link to="/login">Link zur Login Seite</router-link>
-  </div>
-</main>
-
-<!-- Footer -->
-<footer>
-  <AppFooter />
-</footer>
+  <!-- Header -->
+  <header>
+    <AppHeader />
+  </header>
+  
+  <!-- Main -->
+  <main>
+    <div class="haupt">
+      <h1>Deine Kinesiologie-Praxis in Hedingen und Urdorf</h1>
+      <router-link to="/login">Link zur Login Seite</router-link>
+    </div>
+  </main>
+  
+  <!-- Footer -->
+  <footer>
+    <AppFooter />
+  </footer>
 </template>
 
 <style scoped>
+main {
+  background-color: var(--color-background-main);
+}
+
 h1 {
-  color: red;
-  background-color: var(--color-heading-1);
+  color: var(--color-heading-1);
+  background-color: var(--color-background-heading);
 }
 </style>
