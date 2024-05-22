ac6ee7e3db1f3f75bf0668b9f075de840ffcff82
rgrttg <ottiger@outlook.de>
2024-05-22 14:33:09 +0200

    AppHeader hinzugefügt

---
+++ resources/js/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="kopfzeile">
+    <div class="logo">
+      <img alt="Website-Logo" src="" height="180"/>
+    </div>
+    <div class="links">
+      <div>
+        Home
+      </div>
+      <div>
+        Termin buchen
+      </div>
+      <div>
+        Über mich
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.kopfzeile {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: center;
+  background-color: var(--color-background-header);
+}
+.links {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: right;
+}
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,13 +1,14 @@
 <script setup>
 import { ref } from 'vue'
-import AppFooter from '@/components/AppFooter.vue'
+import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
 
 </script>
 
 <template>
 <!-- Header -->
 <header>
-
+  <AppHeader />
 </header>
 
 <!-- Main -->
