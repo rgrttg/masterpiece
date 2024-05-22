c81483ce646f300d6b66c2e35d27cce45721aa5a
rgrttg <ottiger@outlook.de>
2024-05-22 12:33:13 +0200

    background color h1

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,18 +3,28 @@ import { ref } from 'vue'
 </script>
 
 <template>
-    <!-- Header -->
+<!-- Header -->
+<header>
 
-    <!-- Main -->
-    <div>
-        <h1>Startseite</h1>
-        <router-link to="/login">Link zur Login Seite</router-link>
-    </div>
+</header>
 
-    <!-- Footer -->
+<!-- Main -->
+<main>
+  <div>
+    <h1>Deine Kinesiologie-Praxis in Hedingen und Urdorf</h1>
+    <router-link to="/login">Link zur Login Seite</router-link>
+  </div>
+</main>
+
+<!-- Footer -->
+<footer>
+
+</footer>
 </template>
 
-<style lang="scss">
+<style scoped>
 h1 {
-    color: red;
-}</style>
+  color: red;
+  background-color: var(--color-heading-1);
+}
+</style>
