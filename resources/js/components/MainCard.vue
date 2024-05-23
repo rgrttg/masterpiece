e611f97ea07d27df2e499f9dd949f0d6efca2ed4
rgrttg <ottiger@outlook.de>
2024-05-23 16:52:41 +0200

    h2 hinzugefügt

---
+++ resources/js/components/MainCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="haupt">
+    <div id="foto">
+      Foto
+    </div>
+    <div id="text">
+      <h2>ik-k Integrative Kinesiologie Kalbermatter</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.haupt {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: start;
+  
+  background-color: var(--color-background-main);
+  min-height: 320px;
+}
+
+</style>
