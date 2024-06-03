da788013226388ff35803aca120b6fe4558a21bf
rgrttg <ottiger@outlook.de>
2024-06-03 15:56:02 +0200

    termin wahl

---
+++ resources/js/pages/TerminWahl.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
+</script>
+
+<template>
+  <!-- Header -->
+  <header>
+    <AppHeader />
+  </header>
+
+  <main>
+    <h1>Termin buchen</h1>
+    <p>Bitte wählen Sie einen Tag aus:</p>
+    <div>
+      <input type="date" id="termin" name="termin" />
+        <button>Termin buchen</button>
+    </div>
+  </main>
+
+<footer>
+  <AppFooter />
+</footer>
+
+</template>
