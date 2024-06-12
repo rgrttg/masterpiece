d1d9351e7d253c7fb6cd67efd055eff7b851677e
rgrttg <ottiger@outlook.de>
2024-06-12 10:18:56 +0200

    HtmlPicker.vue

---
+++ resources/js/components/HtmlPicker.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>HTML-Version</h2>
+    <p>Bitte wählen Sie einen Tag aus:</p>
+    <div>
+      <input type="date" id="termin" name="termin" />
+      <button>Termin buchen</button>
+    </div>
+</template>
