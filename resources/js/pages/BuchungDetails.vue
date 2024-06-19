e0c6baa4d3a7ae7091035b63ce8569ec10b27ef3
rgrttg <ottiger@outlook.de>
2024-06-19 15:30:29 +0200

    Beschreibung hinzugefügt

--- resources/js/pages/BuchungDetails.vue
+++ resources/js/pages/BuchungDetails.vue
@@ -1,3 +1,6 @@
+<!-- Auf der Detailseite ein Formular, das alle Personalien erfasst; 
+mit einem Button "buchen", der die Angaben in der Datenbank zwischenspeichert
+und dann GANZ WICHTIG in den iCloud-Kalender schreibt. -->
 <script setup>
 import AppHeader from '../components/AppHeader.vue';
 import FormCard from '../components/FormCard.vue';
